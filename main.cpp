a7f1b41847eb167a8b0f460237ddbdd826d9c9ca
Ricardo <ricarditomontserrat@gmail.com>
2023-08-11 22:17:26 +0200

    Added default formatter and print of available extensions

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 #define __FILE_LINE__ __FILE__ << ":" << __LINE__ << ":"
 
@@ -27,7 +28,7 @@ private:
     const uint32_t WIDTH = 800;
     const uint32_t HEIGHT = 600;
 
-    GLFWwindow *window;
+    GLFWwindow* window;
     VkInstance instance;
 
     void initWindow()
@@ -54,8 +55,25 @@ private:
 
     void mainLoop()
     {
+        glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+                glfwSetWindowShouldClose(window, GLFW_TRUE);
+            });
+
         while (!glfwWindowShouldClose(window))
             glfwPollEvents();
+    }
+
+    // Remember to free memory, LMAO
+    std::vector<VkExtensionProperties>* getAvailableExtensions()
+    {
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        std::vector<VkExtensionProperties>* extensions = new std::vector<VkExtensionProperties>(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions->data());
+
+        return extensions;
     }
 
     // This is optional, but it helps the driver optimize for our application
@@ -79,15 +97,24 @@ private:
         LOG("Info created");
 
         uint32_t glfwExtensionCount = 0;
-        const char **glfwExtensions;
+        const char** glfwExtensions;
 
         glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        LOG("Required extensions:");
+        for (uint32_t i = 0; i < glfwExtensionCount; i++)
+            LOG('\t' << glfwExtensions[i]);
 
         createInfo.enabledExtensionCount = glfwExtensionCount;
         createInfo.ppEnabledExtensionNames = glfwExtensions;
         createInfo.enabledLayerCount = 0;
 
         LOG("Extensions enabled");
+
+        auto extensions = getAvailableExtensions();
+
+        for (const auto& extension : *extensions)
+            LOG("Available extension: " << extension.extensionName);
 
         if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
             ERROR("Failed to create instance!");
@@ -116,7 +143,7 @@ int main()
         app.run();
         return EXIT_SUCCESS;
     }
-    catch (const std::exception &e)
+    catch (const std::exception& e)
     {
         LOGE(e.what());
         return EXIT_FAILURE;

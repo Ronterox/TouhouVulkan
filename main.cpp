e8da133507f4a6e1b2b0999e53a997830672f5f1
Ricardo <ricarditomontserrat@gmail.com>
2023-08-11 21:37:51 +0200

    API vulkan initialized

--- main.cpp
+++ main.cpp
@@ -5,9 +5,18 @@
 #include <stdexcept>
 #include <cstdlib>
 
-class HelloTriangleApplication {
+#define __FILE_LINE__ __FILE__ << ":" << __LINE__ << ":"
+
+#define LOG(x) std::cout << __FILE_LINE__ << x << std::endl
+#define LOGE(x) std::cerr << __FILE_LINE__ << x << std::endl
+
+#define ERROR(x) throw std::runtime_error(x)
+
+class HelloTriangleApplication
+{
 public:
-    void run() {
+    void run()
+    {
         initWindow();
         initVulkan();
         mainLoop();
@@ -18,44 +27,98 @@ private:
     const uint32_t WIDTH = 800;
     const uint32_t HEIGHT = 600;
 
-    GLFWwindow* window;
+    GLFWwindow *window;
+    VkInstance instance;
 
-    void initWindow() {
-        glfwInit();
+    void initWindow()
+    {
+        if (!glfwInit())
+            LOGE("GLFW Initialization Error!");
 
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
+        LOG("Initializing windows");
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
-    }
-    void initVulkan() {
 
+        if (!window)
+            LOGE("Window creation error!");
+        else
+            LOG("Window initialized!");
     }
 
-    void mainLoop() {
-        while (!glfwWindowShouldClose(window)) {
+    void initVulkan()
+    {
+        createInstance();
+    }
+
+    void mainLoop()
+    {
+        while (!glfwWindowShouldClose(window))
             glfwPollEvents();
-        }
     }
 
-    void cleanup() {
+    // This is optional, but it helps the driver optimize for our application
+    void createInstance()
+    {
+        LOG("Creating instance");
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        LOG("Application info created");
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        LOG("Info created");
+
+        uint32_t glfwExtensionCount = 0;
+        const char **glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        LOG("Extensions enabled");
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+            ERROR("Failed to create instance!");
+        else
+            LOG("Instance created!");
+    }
+
+    void cleanup()
+    {
+        LOG("Cleaning up");
+
         glfwDestroyWindow(window);
+        LOG("Window destroyed");
 
         glfwTerminate();
+        LOG("GLFW terminated");
     }
 };
 
-int main() {
+int main()
+{
     HelloTriangleApplication app;
 
-    std::cout << "Hello, Triangle!" << std::endl;
-
-    try {
+    try
+    {
         app.run();
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
+        return EXIT_SUCCESS;
+    }
+    catch (const std::exception &e)
+    {
+        LOGE(e.what());
         return EXIT_FAILURE;
     }
-
-    return EXIT_SUCCESS;
 }

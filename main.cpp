120e18203ed244a2fe5ab6298200fa425d6d50ac
Ricardo <ricarditomontserrat@gmail.com>
2023-08-17 07:11:41 +0200

    Created swapchain gits

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 // #define NDEBUG
 #include <GLFW/glfw3.h>
 
-#include <iostream>
-#include <vector>
 #include <algorithm>
-#include <cstring>
+#include <iostream>
 #include <optional>
+#include <cstring>
+#include <vector>
+#include <limits>
 #include <map>
 #include <set>
 
@@ -64,6 +65,8 @@ private:
     VkDebugUtilsMessengerEXT debugMessenger;
     VkSurfaceKHR surface;
 
+    VkSwapchainKHR swapChain;
+
     void initWindow()
     {
         if (!glfwInit()) LOGE("GLFW Initialization Error!");
@@ -85,6 +88,7 @@ private:
         createSurface();
         pickPhysicalDevice();
         createLogicalDevice();
+        createSwapChain();
     }
 
     void mainLoop()
@@ -94,6 +98,56 @@ private:
         });
 
         while (!glfwWindowShouldClose(window)) glfwPollEvents();
+    }
+
+    void createSwapChain() {
+        SwapChainSupportDetails swapChainSupport = querySwapChainSupport(physicalDevice);
+
+        VkSurfaceFormatKHR surfaceFormat = chooseSwapSurfaceFormat(swapChainSupport.formats);
+        VkPresentModeKHR presentMode = chooseSwapPresentMode(swapChainSupport.presentModes);
+        VkExtent2D extent = chooseSwapExtent(swapChainSupport.capabilities);
+
+        uint32_t imageCount = swapChainSupport.capabilities.minImageCount + 1;
+
+        if (swapChainSupport.capabilities.maxImageCount > 0 && swapChainSupport.capabilities.maxImageCount < imageCount)
+            imageCount = swapChainSupport.capabilities.maxImageCount;
+        
+        VkSwapchainCreateInfoKHR createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+        createInfo.surface = surface;
+        createInfo.minImageCount = imageCount;
+        createInfo.imageFormat = surfaceFormat.format;
+        createInfo.imageColorSpace = surfaceFormat.colorSpace;
+        createInfo.imageExtent = extent;
+        createInfo.imageArrayLayers = 1;
+        createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+        QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+        uint32_t queueFamilyIndices[] = { indices.graphicsFamily.value(), indices.presentFamily.value() };
+
+        if (indices.graphicsFamily != indices.presentFamily)
+        {
+            createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+            createInfo.queueFamilyIndexCount = 2;
+            createInfo.pQueueFamilyIndices = queueFamilyIndices;
+            LOG("Concurrent sharing mode");
+        }
+        else
+        {
+            createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+            LOG("Exclusive sharing mode");
+        }
+
+        createInfo.preTransform = swapChainSupport.capabilities.currentTransform;
+        createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR; // Ignore channel
+        createInfo.presentMode = presentMode;
+        createInfo.clipped = VK_TRUE; // Ignore obscured pixels
+        createInfo.oldSwapchain = VK_NULL_HANDLE;
+
+        if (vkCreateSwapchainKHR(device, &createInfo, nullptr, &swapChain) != VK_SUCCESS)
+            ERROR("Failed to create swap chain!");
+        
+        LOG("Created swapchain!");
     }
 
     void createSurface() {
@@ -131,7 +185,9 @@ private:
         createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
 
         createInfo.pEnabledFeatures = &deviceFeatures;
-        createInfo.enabledExtensionCount = 0;
+
+        createInfo.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions.size());
+        createInfo.ppEnabledExtensionNames = deviceExtensions.data();
 
         // No longer necessary since it uses the same validation layers as the instance
         // But keep it for old versions of Vulkan
@@ -162,7 +218,7 @@ private:
         LOG("Finding suitable device");
 
         std::multimap<int, VkPhysicalDevice> candidates;
-        for (auto& device : devices)
+        for (const auto& device : devices)
         {
             int score = rateDeviceSuitability(device);
             candidates.insert(std::make_pair(score, device));
@@ -220,7 +276,7 @@ private:
 
         std::set<std::string> requiredExtensions(deviceExtensions.begin(), deviceExtensions.end());
         // TODO: It says performance difference irrelevant
-        for (auto& extension : availableExtensions) {
+        for (const auto& extension : availableExtensions) {
             requiredExtensions.erase(extension.extensionName);
         }
 
@@ -280,6 +336,7 @@ private:
         if (formatCount != 0) {
             details.formats.resize(formatCount);
             vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, details.formats.data());
+            LOG("Formats: " << formatCount);
         }
 
         uint32_t presentModeCount = 0;
@@ -288,9 +345,45 @@ private:
         if (presentModeCount != 0) {
             details.presentModes.resize(presentModeCount);
             vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, details.presentModes.data());
+            LOG("Present modes: " << presentModeCount);
         }
 
         return details;
+    }
+
+    VkSurfaceFormatKHR chooseSwapSurfaceFormat(const list<VkSurfaceFormatKHR>& availableFormats) {
+        for (const auto& availableFormat : availableFormats) {
+            if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB &&
+                availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+                return availableFormat;
+        }
+
+        return availableFormats[0];
+    }
+
+    VkPresentModeKHR chooseSwapPresentMode(const list<VkPresentModeKHR>& availablePresentModes) {
+        for (const auto& availablePresentMode : availablePresentModes) {
+            if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR)
+                return availablePresentMode;
+        }
+        return VK_PRESENT_MODE_FIFO_KHR;
+    }
+
+    VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities) {
+        if (capabilities.currentExtent.width == std::numeric_limits<uint32_t>::max()) {
+            int width, height;
+            glfwGetFramebufferSize(window, &width, &height);
+
+            VkExtent2D actualExtent{ static_cast<uint32_t>(width), static_cast<uint32_t>(height) };
+
+            actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
+            actualExtent.height = std::clamp(actualExtent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
+
+            LOG("Extent (0,0): " << actualExtent.width << 'x' << actualExtent.height);
+            return actualExtent;
+        }
+        LOG("Extent: " << capabilities.currentExtent.width << 'x' << capabilities.currentExtent.height);
+        return capabilities.currentExtent;
     }
 
     QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
@@ -431,6 +524,9 @@ private:
             DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
             LOG("Debug Messenger destroyed!");
         }
+
+        vkDestroySwapchainKHR(device, swapChain, nullptr);
+        LOG("Swapchain destroyed!");
 
         vkDestroyDevice(device, nullptr);
         LOG("Logical Device destroyed!");

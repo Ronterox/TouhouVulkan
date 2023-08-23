98aacea8cb74aea32f23a9e7692d07c7f39ca997
Ricardo <ricarditomontserrat@gmail.com>
2023-08-23 07:32:15 +0200

    Shader modules created

--- main.cpp
+++ main.cpp
@@ -137,7 +137,52 @@ private:
     void createGraphicsPipeline() {
         list<char> vertShaderCode = readFile("shaders/vert.spv");
         list<char> fragShaderCode = readFile("shaders/frag.spv");
-        LOG(vertShaderCode.size());
+
+        LOG("size of vert.spv=" << vertShaderCode.size());
+        LOG("size of frag.spv=" << fragShaderCode.size());
+
+        VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+        VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+
+        LOG("Shader modules created!");
+
+        VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+        vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+
+        vertShaderStageInfo.module = vertShaderModule;
+        vertShaderStageInfo.pName = "main";
+
+        LOG("Vertex shader stage info created!");
+
+        VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+        fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+
+        fragShaderStageInfo.module = fragShaderModule;
+        fragShaderStageInfo.pName = "main";
+
+        LOG("Fragment shader stage info created!");
+
+        VkPipelineShaderStageCreateInfo shaderStages[] = { vertShaderStageInfo, fragShaderStageInfo };
+
+        LOG("Shader stages created!");
+
+        vkDestroyShaderModule(device, fragShaderModule, nullptr);
+        vkDestroyShaderModule(device, vertShaderModule, nullptr);
+    }
+
+    VkShaderModule createShaderModule(const list<char>& code) {
+        VkShaderModuleCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+        createInfo.codeSize = code.size();
+        createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+        VkShaderModule shaderModule;
+        if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS)
+            ERROR("Failed to create shader module!");
+
+        return shaderModule;
     }
 
     void createImageViews() {
